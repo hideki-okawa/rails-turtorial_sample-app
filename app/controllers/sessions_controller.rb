5e56ab848fddf5ac82b678dce07b0b36b94d09ab
Okaki030 <h.okawa0303@gmail.com>
2022-09-23 02:12:31 +0000

    ログイン失敗時にメッセージを表示する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン後にユーザー情報のページにリダイレクトする
+      redirect_to user
+    else
+      # エラーメッセージを表示する
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
 end
