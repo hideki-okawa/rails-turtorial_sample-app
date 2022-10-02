0cde694e7b19dc7b74962a7555116b0ff3cb440c
Okaki030 <h.okawa0303@gmail.com>
2022-10-02 09:38:00 +0000

    ユーザー削除機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   # editとupdateの前に logged_in_user を実行しログインされていなければログインさせる
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   # 別のユーザープロフィールを編集しようとしたらリダイレクトする
   before_action :correct_user,   only: [:edit, :update]
-  
+  # destoryの前に管理者家の確認を行う
+  before_action :admin_user,     only: :destroy
+    
   # GET /users
   def index
     @users = User.paginate(page: params[:page])
@@ -52,6 +54,13 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  # DELETE /user/:id  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
   
   private
 
@@ -75,5 +84,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -55,4 +55,22 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+  
+  # 未ログインで削除すると削除されない
+  test "should redirect destroy when not logged in" do
+    # ユーザー数が変化しないことを確認する
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  # 管理者以外が削除すると削除されない
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
 end
